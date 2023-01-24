edc37a8fd30f9bb79645a92a038a9ef3cc3667a5
PsychicSalad <54758143+PsychicSalad@users.noreply.github.com>
2023-01-24 22:44:31 +0000

    Added actual questions about coding

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,17 +1,48 @@
 // storing questions in array
 
 let questions = [
-  { title: "Question 1", choices: ["a", "b", "c"], answer: "a" },
+  {
+    title: "Commonly used data types do not include",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    answer: "Alerts",
+  },
 
-  { title: "Question 2", choices: ["a", "b", "c"], answer: "b" },
+  {
+    title:
+      "String values must be enclosed within ____ to be stored as variables",
+    choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+    answer: "Quotes",
+  },
 
-  { title: "Question 3", choices: ["a", "b", "c"], answer: "c" },
+  {
+    title: "What does HTML stand for?",
+    choices: [
+      "Hypertext Markup Language",
+      "Hypertext Mark Uppercase Language",
+      "Happy Man Under Lampshade",
+      "Hyper Text Marking Language",
+    ],
+    answer: "Hypertext Markup Language",
+  },
 
-  { title: "Question 4", choices: ["a", "b", "c"], answer: "b" },
+  {
+    title:
+      "A very useful tool used during development for printing content to the debugger is",
+    choices: ["JavaScript", "console.log", "console printer", "debug.log"],
+    answer: "console.log",
+  },
 
-  { title: "Question 5", choices: ["a", "b", "c"], answer: "c" },
-
-  { title: "Question 6", choices: ["a", "b", "c"], answer: "a" },
+  {
+    title: "What is a README?",
+    choices: [
+      "A text file with information about a project and how to install and use it.",
+      "A program that allows you to check for errors in your code.",
+      "A really good book.",
+      "A function that reads your code aloud.",
+    ],
+    answer:
+      "A text file with information about a project and how to install and use it.",
+  },
 ];
 
 console.log(questions);
